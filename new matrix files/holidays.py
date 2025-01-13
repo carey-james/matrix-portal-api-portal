d4451e46a5d1f1a9d1b325f4f8fcb4ec0c09a066
James Carey <57900176+carey-james@users.noreply.github.com>
2025-01-13 17:00:36 -0500

    Rest of basic holiday functionality

--- new matrix files/holidays.py
+++ new matrix files/holidays.py
@@ -1,69 +1,68 @@
-
 holidays = {
 	'2025':{
 		'1':{
-			'1':'New Years Day',
-			'20':'MLK Jr Day',
-			'29':'Lunar New Year'
+			'1':["Happy New","Year's Day!", 0],
+			'20':["MLK", "Jr Day", 1],
+			'29':["Happy Lunar", "New Year!", 2]
 		},
 		'2':{
-			'2':'Groundhog Day',
-			'12':'Lincolns Birthday',
-			'14':'Valentines Day',
-			'17':'Presidents Day'
+			'2':["Groundhog", "Day", 3],
+			'12':["Lincoln's", "Birthday", 4],
+			'14':["Valentine's", "Day", 5],
+			'17':["President's", "Day", 6]
 		},
 		'3':{
-			'4':'Shrove Tuesday',
-			'5':'Ash Wednesday',
-			'9':'Daylight Saving',
-			'15':'Ides of March',
-			'17':'St. Patricks Day'
+			'4':["Shrove", "Tuesday", 7],
+			'5':["Ash", "Wednesday", 8],
+			'9':["Daylight", "Saving", 9],
+			'15':["Ides of", "March", 10],
+			'17':["St Patrick's", "Day", 11]
 		},
 		'4':{
-			'1':'April Fools Day',
-			'18':'Good Friday',
-			'20':'Easter',
-			'22':'Earth Day'
+			'1':["April Fool's", "Day", 12],
+			'18':["Good", "Friday", 13],
+			'20':["Happy","Easter!", 14],
+			'22':["Earth", "Day", 15]
 		},
 		'5':{
-			'1':'May Day',
-			'5':'Cinco de Mayo',
-			'11':'Mothers Day',
-			'26':'Memorial Day'
+			'1':["May", "Day", 16],
+			'5':["Cinco", "de Mayo", 17],
+			'11':["Mother's", "Day", 18],
+			'26':["Memorial", "Day", 19]
 		},
 		'6':{
-			'8':'Pentecost',
-			'14':'Flag Day',
-			'15':'Fathers Day',
-			'19':'Juneteenth'
+			'8':["Pentecost","", 20],
+			'14':["Flag", "Day", 21],
+			'15':["Father's", "Day", 22],
+			'19':["Juneteenth","", 23]
 		},
 		'7':{
-			'1':'Canada Day',
-			'4':'4th of July'
+			'1':["Canada", "Day", 24],
+			'4':["4th", "of July", 25]
 		},
 		'8':{
 
 		},
 		'9':{
-			'1':'Labor Day',
-			'23':'Rosh Hashanah'
+			'1':["Labor", "Day", 26],
+			'23':["Rosh", "Hashanah", 27]
 		},
 		'10':{
-			'2':'Yom Kippur',
-			'13':'Indigenous Peoples Day',
-			'31':'Halloween'
+			'2':["Yom", "Kippur", 28],
+			'13':["Indigenous", "Peoples Day", 29],
+			'31':["Halloween","", 30]
 		},
 		'11':{
-			'2':'Daylight Saving',
-			'4':'Election Day'
-			'11':'Veterans Day',
-			'27':'Thanksgiving',
-			'28':'Black Friday'
+			'2':["Daylight", "Saving", 31],
+			'4':["Election", "Day", 32],
+			'11':["Veteran's", "Day", 33],
+			'27':["Thanksgiving","", 34],
+			'28':["Black", "Friday", 35]
 		},
 		'12':{
-			'1':'Cyber Monday',
-			'25':'Christmas'
-			'31':'New Years Eve'
+			'1':["Cyber", "Monday", 36],
+			'25':["Merry", "Christmas!", 37],
+			'31':["Happy New", "Year's Eve!", 38]
 		}
 	}
 }
